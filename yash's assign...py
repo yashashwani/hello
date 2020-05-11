6d67c4b4c7184bd0de9bdddc693776143f60b819
Yash ashwani <63654783+yashashwani@users.noreply.github.com>
2020-05-11 22:28:24 +0530

    Add files via upload

---
+++ yash's assign...py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+numbers = [12,-7,5,64,-14]
+for numbers in numbers[0:3:2]:
+      print(numbers)
+
+
+# In[5]:
+
+
+numbers = [12,-7,5,64,-14]
+for numbers in numbers[3:4]:
+      print(numbers)
+
+
+# In[6]:
+
+
+numbers = [12,14,-95,3]
+for numbers in numbers[0:2]:
+         print(numbers)
+
+
+# In[8]:
+
+
+numbers = [12,14,-95,3]
+for numbers in numbers[3:4]:
+         print(numbers)
+
+
+# In[ ]:
+
+
+
+
